cc17516164a3c6155c113bb1a97238fb6200e130
hasinimaddula26-tech <hasinimaddula26@gmail.com>
2026-07-04 20:19:16 +0530

    feat: implement expense SQLAlchemy model

---
+++ backend/app/models/expense.py
@@ -0,0 +1,13 @@
+from sqlalchemy import Column, Integer, String, Numeric, Date, DateTime, Text, func
+from ..core.database import Base
+
+class Expense(Base):
+    __tablename__ = "expenses"
+
+    id = Column(Integer, primary_key=True, index=True, autoincrement=True)
+    title = Column(String(100), nullable=False)
+    amount = Column(Numeric(10, 2), nullable=False)
+    category = Column(String(50), default="Others", nullable=False)
+    date = Column(Date, nullable=False)
+    notes = Column(Text, nullable=True)
+    created_at = Column(DateTime, server_default=func.now(), nullable=False)
